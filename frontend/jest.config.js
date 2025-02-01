7173cc2f8dfeb504c3c6a56fbfe73feecfa837f6
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-02-01 23:49:58 +0700

    fix(frontend): jest testing

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,15 @@
-module.exports = {
-  preset: 'ts-jest',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!your-esm-package|another-esm-package)/',
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
