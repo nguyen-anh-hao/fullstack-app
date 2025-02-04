20724e14786f983f1f867d456b7ab0ce45145c16
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-02-04 17:47:45 +0700

    feat: add logger middleware, global interceptors, pipes, and filters

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  // 3, 7. Interceptor
+  app.useGlobalInterceptors(new LoggerInterceptor());
+
+  // 4. Pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 8. Exception Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
